8cb31e97a150e407c11f6401fda1a3134c9fe358
Francis Encina <frencinap@gmail.com>
2022-02-05 11:48:24 -0300

    commitoro

---
+++ assets/js/script.js
@@ -0,0 +1,110 @@
+// https://superheroapi.com/api/10227239002900026/1
+$( () => {
+
+    //captura dato del input
+    $('form').submit( e => {
+        e.preventDefault()
+
+        let superHero = $("input").val()
+
+        //Petición
+        $.ajax({
+            url: `https://superheroapi.com/api.php/10227239002900026/${superHero}`,
+            success: (data) => {
+                console.log(data);
+
+                //datos de la heroCard
+                let name = data.name
+                let publisher = data.biography.publisher
+                let occupation = data.work.occupation
+                let img = data.image.url
+                let height = data.appearance.height.join(' - ')
+                let weight = data.appearance.weight.join(' - ')
+                let conex = Object.values(data.connections)
+                let connections = conex[0]
+                let bio = Object.values(data.biography)
+                let firstApp = bio[4]
+                let alias = bio[2].join()
+
+                //cuerpo de la heroCard
+                $("#heroCard").html(`
+                <div class="card mb-3" >
+                    <div class="row ">
+                        <div class="col col-4">
+                            <img src="${img}" class="img-fluid rounded-start>
+                        </div>
+                        <div class="col col-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${name}</h5>
+                                <p class="card-text">Conexiones: ${connections} </p>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">
+                                        <span class="fst-italic">Publicado por</span> : ${publisher}
+                                    </li>
+                                    <li class="list-group-item">
+                                        <span class="fst-italic">Ocupación</span> : ${occupation}
+                                    </li>
+                                    <li class="list-group-item">
+                                        <span class="fst-italic">Primera Aparición</span> : ${firstApp}
+                                    </li>
+                                    <li class="list-group-item">
+                                        <span class="fst-italic">Altura</span> : ${height}
+                                    </li>
+                                    <li class="list-group-item">
+                                        <span class="fst-italic">Peso</span> : ${weight}
+                                    </li>
+                                    <li class="list-group-item">
+                                        <span class="fst-italic">Alias</span> : ${alias}
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `)
+            },
+            error: (error) => {
+                console.log(error);
+            }
+        })
+
+    })
+    //CanvasJS chart
+    window.onload = function() {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            theme: "light2", // "light1", "light2", "dark1", "dark2"
+            exportEnabled: true,
+            animationEnabled: true,
+            title: {
+                text: `Estadísticas de poder para`  
+            },
+            data: [{
+                type: "pie",
+                startAngle: 25,
+                toolTipContent: "<b>{label}</b>: {y}%",
+                showInLegend: "true",
+                legendText: "{label}",
+                indexLabelFontSize: 16,
+                indexLabel: "{label} - {y}%",
+                dataPoints: [
+                    { y: 51.08, label: "Chrome" },
+                    { y: 27.34, label: "Internet Explorer" },
+                    { y: 10.62, label: "Firefox" },
+                    { y: 5.02, label: "Microsoft Edge" },
+                    { y: 4.07, label: "Safari" },
+                    { y: 1.22, label: "Opera" },
+                ]
+             }]
+        });
+        chart.render();
+
+    }
+     
+
+
+    
+
+})
+
+
